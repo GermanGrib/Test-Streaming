cec3442cce50493c63afe0c961f4ca048ce9d7a7
Gribanov_German <germangribanov@mail.ru>
2024-09-15 12:55:26 +0400

    refactor: parent path

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Navigation />
       <div className="pt-6">Hello, first stream translation</div>
       <iframe
-        src="https://player.twitch.tv/?channel=youmustdie123&parent=localhost&parent=3000/&muted=true"
+        src="https://player.twitch.tv/?channel=youmustdie123&parent=germangrib.github.io/Test-Streaming//&muted=true"
         height="600"
         width="800"
         allowFullScreen
